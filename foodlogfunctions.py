a85448c8e92c0b0f9ca8ad2f9f1b844058279502
Will Hedges <wshedges@outlook.com>
2024-09-12 22:05:49 -0500

    added logic to combine feeding time(s) * if two feeds are within 90 minutes of each other, counts as 1 * logic is well-commented but could use refactoring

--- foodlogfunctions.py
+++ foodlogfunctions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # foodlogfunctions.py - helper functions for davey-food-log.py
 
+from datetime import timedelta
 import re
 
 import pandas as pd
@@ -24,26 +25,48 @@ def generate_food_log_df(fp):
     #       we will then add that row of data to the summary df
     for date in dates:
         date_df = df[df["Date"] == date]
+        time_list = date_df["Time"].tolist()
         volume_list = date_df["Volume"].tolist()
 
-        # TODO combine "close" feedings into one event
-        # look at one row's date and time
-        # calculate the timedelta between this one and the next row
-        # if the timedelta is less than some value, say 90 min
-        # combine the volumes and remove the next row
+        # combine "close" feedings into one event
+        # we can look at one row's time and calculate the timedelta between
+        #   this time and the next one
+        # if the timedelta is less than 90 minutes,
+        #   combine the volumes and remove the "second" value from both the
+        #       time and volume lists
+        for i, time1 in enumerate(time_list):
+            try:
+                j = i + 1
+                time2 = time_list[j]
+                time_difference = timedelta(
+                    hours=time2.hour - time1.hour,
+                    minutes=time2.minute - time1.minute,
+                )
+                if time_difference < timedelta(minutes=90):
+                    volume_list[i] += volume_list[j]
+                    del time_list[j]
+                    del volume_list[j]
 
-        total_volume = str(sum(volume_list))
+            except IndexError:
+                break
+
+        num_feedings = len(volume_list)
+        total_volume = sum(volume_list)
+        vol_per_feed = round(total_volume / num_feedings, 1)
+
         # match with regex to see if we have a hundredths place
         #   if no match, add a zero
+        total_volume = str(total_volume)
         mo = re.match(r"\d*.\d{2}", total_volume)
         if not mo:
-            total_volume += "0"
+            total_volume = total_volume + "0"
 
         newRow = pd.DataFrame(
             {
                 "Date": [date.strftime("%m-%d-%y")],
-                "Feedings": [str(len(volume_list))],
+                "Feedings": [str(num_feedings)],
                 "Volume (oz)": [total_volume],
+                "oz / Feed": [str(vol_per_feed)],
             }
         )
         summary_df = pd.concat([summary_df, newRow], ignore_index=True)
